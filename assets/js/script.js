05088ddb2ad931cb2700851804d3bdecf580509f
Justin Kao <[justinkao010@gmail.com]>
2024-07-18 14:32:09 -0700

    inital dropdown menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,47 @@
 // swiper button click using swiper js
 const swiperEl = document.querySelector('swiper-container');
 const buttonEl = document.querySelector('button');
+const checkboxesEl = document.querySelectorAll('.dropdown-menu input[type="checkbox"]');
+const displayTagsEl = document.querySelector('#selectedTags');
+const errorEl = document.querySelector('#errorTags');
 
-buttonEl.addEventListener('click', () => {
-    swiperEl.swiper.slideNext();
+const maxTags = 3;
+
+// buttonEl.addEventListener('click', () => {
+//     swiperEl.swiper.slideNext();
+// });
+
+//initialize bootstrap dropdown
+$(document).ready(function () {
+    $('.dropdown-toggle').dropdown();
+});
+
+//displays selected tags
+function updateSelectedTags() {
+    let selectedTags = [];
+    let counter = 0;
+
+    //checks each checkbox if filled
+    checkboxesEl.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            //adds checkbox value to selectedTags array
+            selectedTags.push(checkbox.value);
+            counter++;
+        }
+    });
+
+    //alerts user to select up to maxTags
+    if (counter > maxTags) {
+        errorEl.textContent = "Please select only up 3 tags.";
+    } else {
+        errorEl.textContent = "";
+    }
+
+    //dynamically updates tags selected
+    displayTagsEl.textContent = 'Selected Tags: ' + selectedTags.join(', ');
+}
+
+//event listener every time a checkbox is changed
+checkboxesEl.forEach(function (checkbox) {
+    checkbox.addEventListener('change', updateSelectedTags);
 });
