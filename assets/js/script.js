a165a159aa6ba3d5a508864db450868423631639
Justin Kao <[justinkao010@gmail.com]>
2024-07-18 18:43:55 -0700

    dropdown list dynamically created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,40 @@
 // swiper button click using swiper js
 const swiperEl = document.querySelector('swiper-container');
 const buttonEl = document.querySelector('button');
-const checkboxesEl = document.querySelectorAll('.dropdown-menu input[type="checkbox"]');
+const dropdownMenu = document.querySelector('.dropdown-menu');
 const displayTagsEl = document.querySelector('#selectedTags');
 const errorEl = document.querySelector('#errorTags');
 
 const maxTags = 3;
 
+function createDropdown (tags) {
+    tags.forEach(function (tag) {
+        const liTag = document.createElement('li');
+
+        const labelTag = document.createElement('label');
+        labelTag.classList.add('dropdown-item');
+        let labelText = document.createTextNode(` ${tag}`);
+
+        const checkboxEl = document.createElement('input');
+        checkboxEl.type = 'checkbox';
+        checkboxEl.value = tag;
+
+        labelTag.appendChild(checkboxEl);
+        labelTag.appendChild(labelText);
+        liTag.appendChild(labelTag);
+        dropdownMenu.appendChild(liTag);
+    })
+}
+
 // buttonEl.addEventListener('click', () => {
 //     swiperEl.swiper.slideNext();
 // });
-
-//initialize bootstrap dropdown
-$(document).ready(function () {
-    $('.dropdown-toggle').dropdown();
-});
 
 //displays selected tags
 function updateSelectedTags() {
     let selectedTags = [];
     let counter = 0;
+    const checkboxesEl = document.querySelectorAll('.dropdown-menu input[type="checkbox"]');
 
     //checks each checkbox if filled
     checkboxesEl.forEach(function (checkbox) {
@@ -42,6 +57,12 @@ function updateSelectedTags() {
 }
 
 //event listener every time a checkbox is changed
-checkboxesEl.forEach(function (checkbox) {
-    checkbox.addEventListener('change', updateSelectedTags);
+dropdownMenu.addEventListener('change', function(event) {
+    updateSelectedTags();
+})
+
+//jQuery to run on startup
+$(document).ready(function () {
+    $('.dropdown-toggle').dropdown();
+    createDropdown(tags);
 });
